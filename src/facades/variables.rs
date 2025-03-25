d58a7d4cec975f065c849c43dcdcc0023a1e8874
roney ruiz <roneyruizrojas@gmail.com>
2025-03-24 21:31:12 -0600

    learn: inmutable references in borrowing

--- src/facades/variables.rs
+++ src/facades/variables.rs
@@ -11,10 +11,17 @@ pub fn facade() {
     mtb_number = 100;
     println!("after mutable variable: {}", mtb_number);
 
-    
+
     println!("=== Shadowing variables");
     let shdw_number: i32 = 120;
 
     let shdw_number: i32 = shdw_number + 10;
     println!("shadowing variable: {}", shdw_number);
+
+
+    println!("=== Inmutable references in Borrowing");
+    let imt_string: String = String::from("imutable text");
+
+    let imt_ref_string: &String = &imt_string;
+    println!("inmutable reference: {}", imt_ref_string);
 }
