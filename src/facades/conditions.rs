cce3626df0fd122ac9736fa5e65aa577792dbffc
roney ruiz <roneyruizrojas@gmail.com>
2025-03-25 13:33:06 -0600

    learn: simple conditions

---
+++ src/facades/conditions.rs
@@ -0,0 +1,10 @@
+pub fn facade() {
+    println!("=== Simple conditions");
+    let number: i32 = 19;
+
+    if number > 5 {
+        println!("The number is bigger than 5");
+    } else {
+        println!("The number is shorter than 5");
+    }
+}
