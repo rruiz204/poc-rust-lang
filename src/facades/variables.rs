bd020449f187321d3718985589d1f0405ea2f061
roney ruiz <roneyruizrojas@gmail.com>
2025-03-24 21:24:05 -0600

    learn: shadowing variables

--- src/facades/variables.rs
+++ src/facades/variables.rs
@@ -3,11 +3,18 @@ pub fn facade() {
     let imt_number: i32 = 10;
     println!("inmutable variable: {}", imt_number);
 
-    
+
     println!("=== Mutable variables");
     let mut mtb_number: i32 = 90;
     println!("before mutable variable: {}", mtb_number);
 
     mtb_number = 100;
     println!("after mutable variable: {}", mtb_number);
+
+    
+    println!("=== Shadowing variables");
+    let shdw_number: i32 = 120;
+
+    let shdw_number: i32 = shdw_number + 10;
+    println!("shadowing variable: {}", shdw_number);
 }
