4f5f3c300c93be23ffd3c0966c65283b5dc3b743
roney ruiz <roneyruizrojas@gmail.com>
2025-03-26 21:35:09 -0600

    learn: if let matching use

--- src/facades/matching.rs
+++ src/facades/matching.rs
@@ -7,4 +7,10 @@ pub fn facade() {
         3 => println!("third"),
         _ => println!("other number")
     }
+
+    println!("=== If-Let for matching use");
+    let some_value: Option<i32> = Some(18);
+    if let Some(value) = some_value {
+        println!("The value is: {}", value);
+    }
 }
