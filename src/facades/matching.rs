71e14fdb509e55008770186ce6a5eb78078ef54b
roney ruiz <roneyruizrojas@gmail.com>
2025-03-27 11:27:10 -0600

    learn: matching with struct properties

--- src/facades/matching.rs
+++ src/facades/matching.rs
@@ -1,3 +1,8 @@
+struct Point {
+    x: i32,
+    y: i32,
+}
+
 pub fn facade() {
     println!("=== Simple matching use");
     let number: i32 = 3;
@@ -12,5 +17,13 @@ pub fn facade() {
     let some_value: Option<i32> = Some(18);
     if let Some(value) = some_value {
         println!("The value is: {}", value);
+    }
+
+    println!("=== Structs with matching");
+    let point: Point = Point { x: 10, y: 20 };
+    match point {
+        Point { x, y: 0 } => println!("X is in: {}", x),
+        Point { x: 0, y  } => println!("Y is in: {}", y),
+        Point { x, y } => println!("coord are: ({}, {})", x, y)
     }
 }
