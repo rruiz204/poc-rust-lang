010e7591f3c1c8cc6fe5ecb1b3251934a7100aca
roney ruiz <roneyruizrojas@gmail.com>
2025-04-09 22:31:46 -0600

    learn: mutable variables

--- src/facades/core/variables.rs
+++ src/facades/core/variables.rs
@@ -2,4 +2,13 @@ pub fn showcase() {
   println!("=== Inmutable variables");
   let imt_number: i32 = 10;
   println!("inmutable variable: {}", imt_number);
+
+
+  println!("=== Mutable variables");
+  
+  let mut mtb_number: i32 = 90;
+  println!("before mutable variable: {}", mtb_number);
+
+  mtb_number = 100;
+  println!("after mutable variable: {}", mtb_number);
 }
