44cf6f124380187c6e91d0d1cdf5fb2150c894ca
roney ruiz <roneyruizrojas@gmail.com>
2025-04-09 22:17:13 -0600

    chore: remove old files

--- src/facades/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod enums;
-pub mod vectors;
-pub mod structs;
-pub mod matching;
-pub mod datatypes;
-pub mod variables;
-pub mod functions;
-pub mod controlled;
-pub mod exceptions;
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,5 @@
-use crate::facades::variables;
-use crate::facades::datatypes;
-use crate::facades::functions;
-use crate::facades::controlled;
-use crate::facades::matching;
-use crate::facades::structs;
-use crate::facades::vectors;
-use crate::facades::enums;
-use crate::facades::exceptions;
-
 pub mod facades;
 
 fn main() {
-    variables::facade();
-    datatypes::facade();
-    functions::facade();
-    controlled::facade();
-    matching::facade();
-    structs::facade();
-    enums::facade();
-    vectors::facade();
-    exceptions::facade();
+    println!("hello world!!!");
 }
