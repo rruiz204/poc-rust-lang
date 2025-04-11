f364a2d4fa74828e2c3565caea214ecb2dab24a6
roney ruiz <roneyruizrojas@gmail.com>
2025-04-10 22:30:31 -0600

    learn: manage nullable values

--- src/facades/core/conditions.rs
+++ src/facades/core/conditions.rs
@@ -10,4 +10,12 @@ pub fn showcase() {
   println!("=== Ternary Operator");
   let is_adult: bool = if 17 >= 18 { true } else { false };
   println!("permission: {}", is_adult);
+
+
+  println!("=== If Let Expressions");
+  let something: Option<i32> = Some(100);
+
+  if let Some(value) = something {
+    println!("the value is not null: {}", value);
+  }
 }
