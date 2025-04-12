bbbdbbc737558a23b1af609b3e9023ae30dd9e70
roney ruiz <roneyruizrojas@gmail.com>
2025-04-11 20:45:11 -0600

    learn: hashmaps

--- src/facades/collections.rs
+++ src/facades/collections.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub fn showcase() {
   println!("=== Vectors");
   let mut numbers: Vec<i32> = vec![1, 2, 3];
@@ -9,5 +11,17 @@ pub fn showcase() {
   numbers.reverse();
   for number in numbers {
     println!("number: {}", number);
+  }
+
+
+  println!("=== HashMaps");
+  let mut scores: HashMap<String, i32> = HashMap::new();
+
+  scores.insert(String::from("R"), 100);
+  scores.insert(String::from("O"), 200);
+  scores.insert(String::from("N"), 300);
+
+  if let Some(score) = scores.get("R") {
+    println!("Score: {}", score);
   }
 }
